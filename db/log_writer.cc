f235faa2ed324efec54aed2b3acd04ebdcb81a0c
smartkeyerror <smartkeyerror@gmail.com>
2021-08-16 17:24:16 +0800

    :construction: log write

--- db/log_writer.cc
+++ db/log_writer.cc
@@ -32,45 +32,55 @@ Writer::Writer(WritableFile* dest, uint64_t dest_length)
 Writer::~Writer() = default;
 
 Status Writer::AddRecord(const Slice& slice) {
+  /* 获取 WriteBatch 底层 rep_ 的数据 */
   const char* ptr = slice.data();
+  /* 还剩下多少字节需要写入，在写入过程中该值将减少 */
   size_t left = slice.size();
 
   // Fragment the record if necessary and emit it.  Note that if slice
   // is empty, we still want to iterate once to emit a single
   // zero-length record
   Status s;
+  /* 标志位，表示当前日志记录是否是第一次写入 */
   bool begin = true;
   do {
+    /* 计算当前 Block 剩余写入 */
     const int leftover = kBlockSize - block_offset_;
     assert(leftover >= 0);
+    /* 如果当前 Block 不足以写入 Record Header 的话，需要使用新的 Block */
     if (leftover < kHeaderSize) {
       // Switch to a new block
       if (leftover > 0) {
         // Fill the trailer (literal below relies on kHeaderSize being 7)
+        /* 如果还有剩余空间的话，用 0 进行补齐 */
         static_assert(kHeaderSize == 7, "");
         dest_->Append(Slice("\x00\x00\x00\x00\x00\x00", leftover));
       }
+      /* Block 写入偏移量清零，向新的 Block 中写入 */
       block_offset_ = 0;
     }
 
     // Invariant: we never leave < kHeaderSize bytes in a block.
     assert(kBlockSize - block_offset_ - kHeaderSize >= 0);
 
+    /* 计算剩余写入容量 */
     const size_t avail = kBlockSize - block_offset_ - kHeaderSize;
+    /* 计算片段长度 */
     const size_t fragment_length = (left < avail) ? left : avail;
 
     RecordType type;
     const bool end = (left == fragment_length);
-    if (begin && end) {
+    if (begin && end) {   /* 刚好装下 */
       type = kFullType;
-    } else if (begin) {
+    } else if (begin) {   /* 一个 Block 无法装下 */
       type = kFirstType;
-    } else if (end) {
+    } else if (end) {     /* 能装下上一份数据 */
       type = kLastType;
-    } else {
+    } else {              /* 其它的中间状态 */
       type = kMiddleType;
     }
 
+    /* 写入预写日志 */
     s = EmitPhysicalRecord(type, ptr, fragment_length);
     ptr += fragment_length;
     left -= fragment_length;
