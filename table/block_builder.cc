811f6df64bd9f7ed5e04efdaa86a86cd3d1160f2
smartkeyerror <smartkeyerror@gmail.com>
2021-08-17 14:50:24 +0800

    :sparkles: BlockBuilder

--- table/block_builder.cc
+++ table/block_builder.cc
@@ -52,6 +52,7 @@ void BlockBuilder::Reset() {
   last_key_.clear();
 }
 
+/* 返回 Block 未压缩之前的大小 */
 size_t BlockBuilder::CurrentSizeEstimate() const {
   return (buffer_.size() +                       // Raw data buffer
           restarts_.size() * sizeof(uint32_t) +  // Restart array
@@ -59,45 +60,57 @@ size_t BlockBuilder::CurrentSizeEstimate() const {
 }
 
 Slice BlockBuilder::Finish() {
-  // Append restart array
+  /* 压入 restarts_ 数组中的全部内容至 buffer_ */
   for (size_t i = 0; i < restarts_.size(); i++) {
     PutFixed32(&buffer_, restarts_[i]);
   }
+  /* 压入 Restart Points 数量 */
   PutFixed32(&buffer_, restarts_.size());
+  /* 设置结束标志位 */
   finished_ = true;
+  /* 返回完整的 Buffer 内容 */
   return Slice(buffer_);
 }
 
 void BlockBuilder::Add(const Slice& key, const Slice& value) {
+  /* 获取 last_key_ */
   Slice last_key_piece(last_key_);
   assert(!finished_);
   assert(counter_ <= options_->block_restart_interval);
+  /* 要么 buffer_ 为空，要么 key 大于最后一个被添加到 Block 中的 Key */
   assert(buffer_.empty()  // No values yet?
          || options_->comparator->Compare(key, last_key_piece) > 0);
+
   size_t shared = 0;
+
+  /* 如果 counter_ < block_restart_interval 的话，说明还没有到重启点，直接进行前缀压缩处理 */
   if (counter_ < options_->block_restart_interval) {
-    // See how much sharing to do with previous string
+    /* last_key_ 就像链表里面儿的 prev 指针一样，只需要计算当前 User Key 和 last_key_ 有多少重合度即可 */
     const size_t min_length = std::min(last_key_piece.size(), key.size());
+    /* 统计前缀长度 */
     while ((shared < min_length) && (last_key_piece[shared] == key[shared])) {
       shared++;
     }
   } else {
-    // Restart compression
+    /* 此时 counter_ 必然等于 block_restart_interval，需要建立新的重启点 */
     restarts_.push_back(buffer_.size());
     counter_ = 0;
   }
+
+  /* 获取 key 和 last_key_ 的非共享长度 */
   const size_t non_shared = key.size() - shared;
 
-  // Add "<shared><non_shared><value_size>" to buffer_
+  /* 使用变长编码，将 "<shared><non_shared><value_size>" 写入 buffer_ */
   PutVarint32(&buffer_, shared);
   PutVarint32(&buffer_, non_shared);
   PutVarint32(&buffer_, value.size());
 
-  // Add string delta to buffer_ followed by value
+  /* 将 User Key 非共享内容压入 buffer_ */
   buffer_.append(key.data() + shared, non_shared);
+  /* 将完整的 Value 压入 buffer_ */
   buffer_.append(value.data(), value.size());
 
-  // Update state
+  /* 更新 last_key_ 为当前 User Key */
   last_key_.resize(shared);
   last_key_.append(key.data() + shared, non_shared);
   assert(Slice(last_key_) == key);
--- table/block_builder.h
+++ table/block_builder.h
@@ -21,32 +21,28 @@ class BlockBuilder {
   BlockBuilder(const BlockBuilder&) = delete;
   BlockBuilder& operator=(const BlockBuilder&) = delete;
 
-  // Reset the contents as if the BlockBuilder was just constructed.
+  /* 清空 */
   void Reset();
 
-  // REQUIRES: Finish() has not been called since the last call to Reset().
-  // REQUIRES: key is larger than any previously added key
+  /* 添加一个 Key-Value 对 */
   void Add(const Slice& key, const Slice& value);
 
-  // Finish building the block and return a slice that refers to the
-  // block contents.  The returned slice will remain valid for the
-  // lifetime of this builder or until Reset() is called.
+  /* 完成 Block 的构建，压入重启点信息，并返回 buffer_，设置 finished_ 为 true */
   Slice Finish();
 
-  // Returns an estimate of the current (uncompressed) size of the block
-  // we are building.
+  /* 返回 Block 的预估大小 */
   size_t CurrentSizeEstimate() const;
 
-  // Return true iff no entries have been added since the last Reset()
+  /* 判断 buffer_ 是否为空 */
   bool empty() const { return buffer_.empty(); }
 
  private:
-  const Options* options_;
-  std::string buffer_;              // Destination buffer
-  std::vector<uint32_t> restarts_;  // Restart points
-  int counter_;                     // Number of entries emitted since restart
-  bool finished_;                   // Has Finish() been called?
-  std::string last_key_;
+  const Options* options_;          /* Options 对象 */
+  std::string buffer_;              /* User Space 缓冲区 */
+  std::vector<uint32_t> restarts_;  /* Restart Points 数组 */
+  int counter_;                     /* Entry 计数器，用于重启点的计算 */
+  bool finished_;                   /* 是否已经调用了 Finish() 方法 */
+  std::string last_key_;            /* 最后添加的 Key */
 };
 
 }  // namespace leveldb
